4c2b8a071a17c1bb7a68e8eafd2670fb664ab663
Mohan Bhojaraja <mohan.bhojaraja@versent.com.au>
2026-04-28 13:21:37 +1000

    Initial Commit

---
+++ BrewDog/BrewDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewDogApp.swift
+//  BrewDog
+//
+//  Created by Mohan Bhojaraja on 28/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct BrewDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
